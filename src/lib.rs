76ff2b1aaa76958711af1c33095e70139ec8528e
cryptoAtwill <willes.lau@protocol.ai>
2022-11-06 21:50:30 +0800

    expose subnet id

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ pub use self::cross::{is_bottomup, CrossMsgs, IPCMsgType, StorableMsg};
 pub use self::state::*;
 pub use self::subnet::*;
 pub use self::types::*;
-use crate::subnet_id::SubnetID;
+pub use crate::subnet_id::SubnetID;
 
 #[cfg(feature = "fil-actor")]
 fil_actors_runtime::wasm_trampoline!(Actor);
