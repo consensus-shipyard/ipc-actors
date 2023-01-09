sergefdrv/ipc-atomic-execution

Implement primitives useful for execution actors

src/lib.rs
@@ -10,6 +10,8 @@ use ipc_gateway::{ApplyMsgParams, CrossMsg, StorableMsg};
 use num_derive::FromPrimitive;
 use num_traits::{FromPrimitive, Zero};
 
+pub use crate::atomic::{AtomicExecID, AtomicInput, AtomicInputID, AtomicOutput};
+pub use crate::atomic::{AtomicExecRegistry, AtomicInputState, LockableState};
 pub use crate::types::{ConstructorParams, PreCommitParams, RevokeParams};
 
 mod atomic;

consensus-shipyard/ipc-actors

src/lib.rs
@@ -10,9 +10,8 @@ use ipc_gateway::{ApplyMsgParams, CrossMsg, StorableMsg};
 use num_derive::FromPrimitive;
 use num_traits::{FromPrimitive, Zero};
 
-pub use crate::types::{ConstructorParams, PreCommitParams, RevokeParams};
+pub use crate::types::{AtomicExecID, ConstructorParams, PreCommitParams, RevokeParams};
 
-mod atomic;
 mod state;
 mod types;
 