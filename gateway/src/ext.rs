1e5b90f150a5983775647e5ab8d864f38e5a0192
adlrocha <adlrocha@tutamail.com>
2023-02-16 13:45:56 +0100

    mint circ.supply without modified reward actor (#52)

--- gateway/src/ext.rs
+++ gateway/src/ext.rs
@@ -1,17 +1,3 @@
 pub mod account {
     pub const PUBKEY_ADDRESS_METHOD: u64 = 2;
 }
-
-pub mod reward {
-    use fvm_shared::address::Address;
-    use fvm_shared::econ::TokenAmount;
-    use serde::{Deserialize, Serialize};
-
-    pub const EXTERNAL_FUNDING_METHOD: u64 = 5;
-
-    #[derive(Serialize, Deserialize, Clone)]
-    pub struct FundingParams {
-        pub value: TokenAmount,
-        pub addr: Address,
-    }
-}
